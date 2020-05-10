86de0851d93642984493dc1c0008baff7bc5ce03
Ingrid <ihorng@uci.edu>
2020-05-10 16:33:07 -0700

    updated server.js, lobby.html, and main.js

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ function getURLParameters(whichParam)
 {
 	var pageURL= window.location.search.substring(1);
 	var pageURLVariables=pageURL.split('&');
-	for(var i=0; i < pageURLVariables.length; i++){
+	for(var i=0; i <pageURLVariables.length; i++){
 		var parameterName = pageURLVariables[i].split('=');
 		if(parameterName[0] == whichParam){
 			return parameterName [1];
@@ -18,7 +18,7 @@ if ('underfined'== typeof username || !username){
 	username = 'Anonymous_'+Math.random();
 }
 
-$('#messages').append('<h4>'+username+' </h4>');
+var chat_room = 'One_Room';
 
 /* Connect to the socket server */
 
@@ -27,3 +27,41 @@ var socket = io.connect();
 socket.on('log',function(array){ 
 	console.log.apply(console,array);
 });
+
+socket.on('join_room_response',function(payload){
+	if(payload.result == 'fail'){
+    alert(payload.message);
+	return;
+}
+$('#messages').append('<p>New user joined the room: '+payload.username+'</p>');
+
+});
+
+socket.on('send_message_response',function(payload){
+	if(payload.result == 'fail'){
+    alert(payload.message);
+	return;
+
+}
+
+$('#messages').append('<p><b>'+payload.username+'says:</b> '+payload.message+'</p>');
+
+});
+
+function send_message() {
+	var payload = {};
+	payload.room = chat_room;
+	payload.username = username;
+	payload.message= $('#send_message_holder').val();
+	console.log('*** Client Log Message:\'send_message\' payload: '+JSON.stringify(payload));
+	socket.emit('send_message',payload);
+}
+
+$ (function(){
+	var payload = {};
+	payload.room = chat_room;
+	payload.username = username;
+	console.log('*** Client Log Message:\'join_room\' payload: '+JSON.stringify(payload));
+	socket.emit('join_room',payload);
+});
+
